79aa1c19813f678cd9a5b1138ff0eec84bab16ba
Temitope Ayodele <39211865+temmietope@users.noreply.github.com>
2019-11-09 23:40:20 +0100

    better algorithm used

--- Calculate the sum of numbers less than 10 in an array/index.js
+++ Calculate the sum of numbers less than 10 in an array/index.js
@@ -1,21 +1,21 @@
 //Calculate the sum of numbers less than 10 in an array
 
-
 //SOLUTION
 
 //Function
-const Calc = input => {
+const Calc = (input, range) => {
   if (!Array.isArray(input)) {  //Check if argument passed is an array
     return console.log("Pass in an array");  //Return if not an array
   }
-  const newArray = input.filter(a => a <= 10);  // filter out elements of the array greater than 10
-
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;  //Create a reducer function
-  const sum = newArray.reduce(reducer);
-  return console.log(sum);
+  let total = 0;
+  for (let i = 0; i <= input.length; i++) {
+    if (input[i] <= range) {
+      total = total + input[i];
+    } else continue;
+  }
+  console.log(total);
 };
 
-
 let arr = [1, 3, 5, 19, 3, 8, 10, 3, 4, 5];
-Calc(arr);
- //RESULT =42
+Calc(arr, 10);
+//RESULT =42
