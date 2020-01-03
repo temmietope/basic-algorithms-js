ea55a4970d0b15e7b7d5e5113f1f1295060cc020
Temitope Ayodele <39211865+temmietope@users.noreply.github.com>
2020-01-03 08:27:15 +0100

    reverse string solution

---
+++ Reverse a string/index.js
@@ -0,0 +1,12 @@
+//Create a function that reverses a string
+function FirstReverse(str) {
+  // code goes here
+  let rev_string = str
+    .split("")
+    .reverse()
+    .join("");
+  return rev_string;
+}
+
+console.log(FirstReverse(code));
+//RESULT: edoc
