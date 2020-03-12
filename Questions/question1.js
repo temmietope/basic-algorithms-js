d29fa3fd09c814f36185c70179c6cb853db5e156
temmietope <temmieayodele@gmail.com>
2020-03-12 17:38:41 +0100

    new question alert

---
+++ Questions/question1.js
@@ -0,0 +1,21 @@
+/**
+ QUESTION
+All the students taking a module have filled in a questionnaire which contained 10 questions. Each question was answered with the same scale:
+Strongly Disagree [ ] Disagree [ ] Neutral [ ] Agree [ ] Strongly Agree [ ]
+The students answers have been coded from 1 (Strongly Disagree) to 5 (Strongly Agree) so that the 10 answers from each student are coded as 10 numbers from 1 to 5. This data is now ready to be analysed.
+
+You are to write a program to read and analyse the data from 50 students. The students' answers are to be given scores [-5, -2, 0, 2, 5] so that "Strongly Disagree" is scored -5 and "Strongly Agree" is scored 5.
+The average score for each question should be calculated and reported. Finally, the question with the lowest total score should be reported**/
+
+
+const gradeEachStudent = (studentsData)=>{
+    for (let student = 1; student <= 50; student++) {
+        for (let question = 1; question <= 10; question++) {
+            
+        }
+      }
+}
+const gradeQuestionnaire = studentsData => {
+  let scorePerOption = [-5, -2, 0, 2, 5];
+  
+};
